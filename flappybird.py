1e4c8bc49194e128c8dee67dd81d5b29cfe6f6cd
naszifnaaz <naszifnaaz@gmail.com>
2020-11-06 18:58:14 +0530

    basic pygame setup

--- flappybird.py
+++ flappybird.py
@@ -1,1 +1,26 @@
-#FlappyBird
+#FlappyBird using pygame
+
+import pygame
+import sys
+
+#game constants
+WIDTH = 576
+HEIGHT = 700
+FPS = 120
+run = True
+
+pygame.init()
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Flappy Bird")
+clock = pygame.time.Clock()
+
+while run:
+    
+    #setting events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(FPS)
