cf006eaf3db4755d65250ce14ceb6554c157a06b
naszifnaaz <naszifnaaz@gmail.com>
2020-11-06 20:53:58 +0530

    added bird

--- flappybird.py
+++ flappybird.py
@@ -12,11 +12,17 @@ win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Flappy Bird")
 clock = pygame.time.Clock()
 
-#drawing background
+#loading assets
 bg_surface = pygame.image.load('assets/images/sprites/Background.png').convert()
 bg_surface = pygame.transform.scale(bg_surface,(WIDTH,HEIGHT))
+
 floor_surface = pygame.image.load('assets/images/sprites/Floor.png').convert()
 floor_surface = pygame.transform.scale2x(floor_surface)
+
+bird_surface = pygame.image.load('assets/images/sprites/Bird_mid.png').convert()
+bird_surface = pygame.transform.scale2x(bird_surface)
+bird_rect = bird_surface.get_rect(center = (100, 400))
+
 
 #drawing floor
 def draw_floor():
@@ -33,6 +39,7 @@ while run:
             sys.exit()
 
     win.blit(bg_surface,(0,0))
+    win.blit(bird_surface,bird_rect)
     dynamic_floor -= 1
     draw_floor()
     if dynamic_floor <= -500:
