465534a67d3486eb443d9d4853252c8d7316d4c7
naszifnaaz <naszifnaaz@gmail.com>
2020-11-06 19:12:59 +0530

    drawing bg

--- flappybird.py
+++ flappybird.py
@@ -5,15 +5,22 @@ import sys
 
 #game constants
 WIDTH = 576
-HEIGHT = 700
+HEIGHT = 800
 FPS = 120
 run = True
 
+#pygame initialisation
 pygame.init()
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Flappy Bird")
 clock = pygame.time.Clock()
 
+#drawing background
+bg_surface = pygame.image.load('assets/images/sprites/Background.png').convert()
+bg_surface = pygame.transform.scale2x(bg_surface)
+win.blit(bg_surface,(0,0))
+
+#game loop
 while run:
     
     #setting events
